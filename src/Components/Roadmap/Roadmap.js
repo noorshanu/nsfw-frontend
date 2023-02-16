e7abbcfb509ee37d33f4e28e8cc4cb63ce1b0363
draco <122390064+Darco786@users.noreply.github.com>
2023-02-17 01:57:21 +0530

    new

--- src/Components/Roadmap/Roadmap.js
+++ src/Components/Roadmap/Roadmap.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import BackBG from '../../Assets/bg.png'
-import './Road.css'
+import React from "react";
+import BackBG from "../../Assets/bg.png";
+import "./Road.css";
 
 function Roadmap() {
   return (
     <>
-    <img src={BackBG} alt="" className='road-dapp'/>
-    <section className='Roadmap'>
-    <div className="container">
+      <img src={BackBG} alt="" className="road-dapp" />
+      <section className="Roadmap">
+        <div className="container">
           <div className="header-text text-center">
             <h2>Roadmap</h2>
             <hr className="low-hr" />
           </div>
 
-          <div className='first-b'>
-            <div className='road_num'><h1>01</h1>
-
+          <div className="first-b">
+            <div className="road_num">
+              <h1>01</h1>
             </div>
-            <div>
-              <div className='road_details'>
+            <div className="road-d-box">
+              <div className="road_details">
                 <ul>
                   <li>Creation of concept</li>
                   <li>Creation of Website & Tokenomics</li>
@@ -29,15 +29,77 @@ function Roadmap() {
                   <li>Community Contests & Hype Creation</li>
                 </ul>
               </div>
+            </div>
+          </div>
 
+          <div className="first-b">
+            <div className="road-d-box">
+              <div className="road_details">
+                <ul>
+                  <li>Creation of concept</li>
+                  <li>Creation of Website & Tokenomics</li>
+                  <li>Community Creation</li>
+                  <li>Launch of Image Generative TG Bot</li>
+                  <li>launch of ERC 20 Token</li>
+                  <li>Ecosystem Showcase</li>
+                  <li>Community Contests & Hype Creation</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="road_num">
+              <h1>02</h1>
+            </div>
+          </div>
+
+
+
+
+          <div className="first-b">
+            <div className="road_num">
+              <h1>03</h1>
+            </div>
+            <div className="road-d-box">
+              <div className="road_details">
+                <ul>
+                  <li>Creation of concept</li>
+                  <li>Creation of Website & Tokenomics</li>
+                  <li>Community Creation</li>
+                  <li>Launch of Image Generative TG Bot</li>
+                  <li>launch of ERC 20 Token</li>
+                  <li>Ecosystem Showcase</li>
+                  <li>Community Contests & Hype Creation</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div className="first-b">
+            <div className="road-d-box">
+              <div className="road_details">
+                <ul>
+                  <li>Creation of concept</li>
+                  <li>Creation of Website & Tokenomics</li>
+                  <li>Community Creation</li>
+                  <li>Launch of Image Generative TG Bot</li>
+                  <li>launch of ERC 20 Token</li>
+                  <li>Ecosystem Showcase</li>
+                  <li>Community Contests & Hype Creation</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="road_num">
+              <h1>04</h1>
             </div>
           </div>
 
         </div>
-
-    </section>
+      </section>
     </>
-  )
+  );
 }
 
-export default Roadmap
+export default Roadmap;
