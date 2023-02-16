7b712234c4efd874e5da2b254491025236f68479
draco <122390064+Darco786@users.noreply.github.com>
2023-02-16 16:27:38 +0530

    new

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -20,7 +20,57 @@ function Hero() {
                    
                 </div>
                 <div className='col-md-6'>
-                    <div>
+                    <div className='top-box-hero'>
+                        <div className='hero-demo-box'>
+                            <div className='eth-box'>
+                                <h1>2.01 ETH</h1>
+
+                            </div>
+
+                            <div className='qr-box'>
+                                <h2>
+                                    <span></span>
+                                    KAIJIN
+                                </h2>
+                                <p>1023</p>
+                            </div>
+
+                        </div>
+
+                        <div className='hero-demo-box'>
+                            <div className='eth-box'>
+                                <h1>2.01 ETH</h1>
+
+                            </div>
+
+                            <div className='qr-box'>
+                                <h2>
+                                    <span></span>
+                                    KAIJIN
+                                </h2>
+                                <p>1023</p>
+                            </div>
+
+                        </div>
+
+
+
+                        <div className='hero-demo-box'>
+                            <div className='eth-box'>
+                                <h1>2.01 ETH</h1>
+
+                            </div>
+
+                            <div className='qr-box'>
+                                <h2>
+                                    <span></span>
+                                    KAIJIN
+                                </h2>
+                                <p>1023</p>
+                            </div>
+
+                        </div>
+
 
                     </div>
                 </div>
