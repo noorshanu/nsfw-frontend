4d6f72efbd5f8c43f7009ffe3ddbefc12e54956d
draco <122390064+Darco786@users.noreply.github.com>
2023-02-16 15:02:50 +0530

    new

--- src/Components/Ecosystem/Ecosystem.js
+++ src/Components/Ecosystem/Ecosystem.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import './eco.css'
 import EcoList from './EcoList'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Pagination } from "swiper";
+
+import Nft1 from '../../Assets/Eco/gal/img1.png'
+import Nft2 from '../../Assets/Eco/gal/img2.svg'
+
+
+
 
 function Ecosystem() {
   return (
@@ -14,7 +27,25 @@ function Ecosystem() {
             </div>
             <div className='eco-grid'>
 
-                
+            <Swiper
+        slidesPerView={5}
+        spaceBetween={1}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src={Nft1} alt=""/></SwiperSlide>
+        <SwiperSlide><img src={Nft1} alt=""/></SwiperSlide>
+        <SwiperSlide><img src={Nft1} alt=""/></SwiperSlide>
+        <SwiperSlide><img src={Nft1} alt=""/></SwiperSlide>
+        <SwiperSlide><img src={Nft1} alt=""/></SwiperSlide>
+        <SwiperSlide><img src={Nft1} alt=""/></SwiperSlide>
+        <SwiperSlide><img src={Nft1} alt=""/></SwiperSlide>
+        <SwiperSlide><img src={Nft1} alt=""/></SwiperSlide>
+        <SwiperSlide><img src={Nft2} alt=""/></SwiperSlide>
+      </Swiper>
             </div>
 
         </div>
