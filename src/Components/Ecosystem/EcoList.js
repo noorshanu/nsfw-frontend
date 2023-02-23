e5665ebcf287d651cd788e7093e289364b8e40d3
draco <122390064+Darco786@users.noreply.github.com>
2023-02-23 17:30:41 +0530

    new

--- src/Components/Ecosystem/EcoList.js
+++ src/Components/Ecosystem/EcoList.js
@@ -7,6 +7,14 @@ import last from "../../Assets/Eco/last.png";
 
 function EcoList() {
   const [display, setDisplay] = useState("notdisplayed");
+
+  const [display1, setDisplay1] = useState("notdisplayed");
+  const [display2, setDisplay2] = useState("notdisplayed");
+  const [display3, setDisplay3] = useState("notdisplayed");
+  const [display4, setDisplay4] = useState("notdisplayed");
+
+
+
   const showButton = (e) => {
     e.preventDefault();
     setDisplay("displayed");
@@ -16,6 +24,57 @@ function EcoList() {
     e.preventDefault();
     setDisplay("notdisplayed");
   };
+
+
+
+  const showButton1 = (e) => {
+    e.preventDefault();
+    setDisplay1("displayed");
+  };
+
+  const hideButton1 = (e) => {
+    e.preventDefault();
+    setDisplay1("notdisplayed");
+  };
+
+
+  const showButton2 = (e) => {
+    e.preventDefault();
+    setDisplay2("displayed");
+  };
+
+  const hideButton2 = (e) => {
+    e.preventDefault();
+    setDisplay2("notdisplayed");
+  };
+
+
+
+
+  const showButton3 = (e) => {
+    e.preventDefault();
+    setDisplay3("displayed");
+  };
+
+  const hideButton3 = (e) => {
+    e.preventDefault();
+    setDisplay3("notdisplayed");
+  };
+
+
+
+  const showButton4 = (e) => {
+    e.preventDefault();
+    setDisplay4("displayed");
+  };
+
+  const hideButton4 = (e) => {
+    e.preventDefault();
+    setDisplay4("notdisplayed");
+  };
+
+
+
   return (
     <section className="eco-list">
       <div className="container">
@@ -41,27 +100,32 @@ function EcoList() {
             </p>
           </div>
 
-          <div className="eco-box" data-aos="fade-up" data-aos-duration="1000">
+          <div className="eco-box" data-aos="fade-up" data-aos-duration="1000"
+           onMouseEnter={(e) => showButton1(e)}
+           onMouseLeave={(e) => hideButton1(e)}
+          >
             <div className="eco-icon-box">
               <img src={Play} alt="" />
             </div>
             <p>NSFW Video Generator</p>
           </div>
-          <div className={`box-2-eco  ${display} `}>
+          <div className={`box-1-eco  ${display1} `}>
             <p>
               NSFW Image generator is where you can start building your NSFW
               dreams, we will have both webapp as well as telegram bot to
               generate NSFW images.
             </p>
           </div>
-          <div className="eco-box" data-aos="fade-up" data-aos-duration="2000">
+          <div className="eco-box" data-aos="fade-up" data-aos-duration="2000"
+             onMouseEnter={(e) => showButton2(e)}
+             onMouseLeave={(e) => hideButton2(e)}>
             <div className="eco-icon-box">
               <img src={Nft} alt="" />
             </div>
             <p>One Click NFT Minter</p>
           </div>
 
-          <div className={`box-3-eco  ${display} `}>
+          <div className={`box-1-eco  ${display2} `}>
             <p>
               NSFW Image generator is where you can start building your NSFW
               dreams, we will have both webapp as well as telegram bot to
@@ -69,14 +133,16 @@ function EcoList() {
             </p>
           </div>
 
-          <div className="eco-box" data-aos="fade-up" data-aos-duration="2000">
+          <div className="eco-box" data-aos="fade-up" data-aos-duration="2000"
+             onMouseEnter={(e) => showButton3(e)}
+             onMouseLeave={(e) => hideButton3(e)}>
             <div className="eco-icon-box">
               <img src={Nft2} alt="" />
             </div>
             <p>NFT Marketplace</p>
           </div>
 
-          <div className={`box-4-eco  ${display} `}>
+          <div className={`box-1-eco  ${display3} `}>
             <p>
               NSFW Image generator is where you can start building your NSFW
               dreams, we will have both webapp as well as telegram bot to
@@ -84,13 +150,15 @@ function EcoList() {
             </p>
           </div>
 
-          <div className="eco-box" data-aos="fade-up" data-aos-duration="3000">
+          <div className="eco-box" data-aos="fade-up" data-aos-duration="3000"
+             onMouseEnter={(e) => showButton4(e)}
+             onMouseLeave={(e) => hideButton4(e)}>
             <div className="eco-icon-box">
               <img src={last} alt="" />
             </div>
             <p>Governance Dao</p>
           </div>
-          <div className={`box-5-eco  ${display} `}>
+          <div className={`box-1-eco  ${display4} `}>
             <p>
               NSFW Image generator is where you can start building your NSFW
               dreams, we will have both webapp as well as telegram bot to
