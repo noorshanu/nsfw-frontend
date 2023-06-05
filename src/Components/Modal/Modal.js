537609db2ddf83328ce58398a41b0dc88f785130
unknown <anas.452111@gmail.com>
2023-06-05 11:09:04 +0500

    Modal fixed

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -78,7 +78,7 @@ export default function Modal({showContent, setShowContent}) {
                   </button>
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    className="inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900  mt-2 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                     onClick={() => {
                         navigate("/notallowed")
                     }}
